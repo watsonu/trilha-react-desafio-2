e9b10afd2f9899d4ace3b6a3106c10471965f51f
Watson Breno <watsonbreno2014@gmail.com>
2024-10-29 11:31:27 -0300

    Adicionado o método removeRepo

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,51 +1,39 @@
-
 import { useState } from 'react';
-import gitLogo from '../assets/github.png'
+import gitLogo from '../assets/github.png';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import ItemRepo from '../components/ItemRepo';
 import { api } from '../services/api';
-
 import { Container } from './styles';
 
 function App() {
-
   const [currentRepo, setCurrentRepo] = useState('');
   const [repos, setRepos] = useState([]);
 
-
   const handleSearchRepo = async () => {
-
-    const {data} = await api.get(`repos/${currentRepo}`)
-
-    if(data.id){
-
+    const { data } = await api.get(`repos/${currentRepo}`);
+    if (data.id) {
       const isExist = repos.find(repo => repo.id === data.id);
-
-      if(!isExist){
+      if (!isExist) {
         setRepos(prev => [...prev, data]);
-        setCurrentRepo('')
-        return
+        setCurrentRepo('');
+        return;
       }
-
     }
-    alert('Repositório não encontrado')
-
+    alert('Repositório não encontrado');
   }
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
+    const filteredRepos = repos.filter(repo => repo.id !== id);
+    setRepos(filteredRepos);
   }
-
 
   return (
     <Container>
-      <img src={gitLogo} width={72} height={72} alt="github logo"/>
+      <img src={gitLogo} width={72} height={72} alt="github logo" />
       <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
-      <Button onClick={handleSearchRepo}/>
-      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+      <Button onClick={handleSearchRepo} />
+      {repos.map(repo => <ItemRepo key={repo.id} handleRemoveRepo={handleRemoveRepo} repo={repo} />)}
     </Container>
   );
 }
